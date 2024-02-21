0d3dad5117c0ccffab3a03aba394acb173434d30
Michele Ceccacci <michelececcacci1@gmail.com>
2024-02-21 15:38:05 +0100

    first version of scripts

---
+++ Progetto-HPC-2023-2024/src/strong.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Questo script richiede 4 parametri per essere eseguito:
+# 1 - il nome del programma da eseguire
+# 2 - numero di ripetizioni
+# 3-4 - La grandezza del problema da eseguire
+
+
+# Lo script esegue il programma con un numero di core variabile 
+# da 1 al numero di core disponibili sulla macchina (estremi inclusi); 
+# ogni esecuzione considera sempre la stessa dimensione dell'input, 
+# quindi i tempi misurati possono essere usati per calcolare speedup 
+# e strong scaling efficiency.
+# vengono stampati a video i tempi di esecuzione di tutte le esecuzioni.
+
+
+PROG=$1
+REP=$2
+PROB_SIZE=$3 # problem size 
+IT=$4 # problem iterations
+
+
+if [ ! -f "$PROG" ]; then
+    echo
+    echo "Non trovo il programma $PROG."
+    echo
+    exit 1
+fi
+
+echo -n "p\t"
+
+for ((t=i; i <= REP; i++)); do
+echo -n "t{$t}\t"
+done
+echo ""
+
+CORES=$(cat /proc/cpuinfo | grep processor | wc -l) # number of cores
+
+if  echo "$PROG" | grep -Eq 'omp' ; then
+    TYPE=0;
+else
+    TYPE=1;
+fi
+
+if [ "$TYPE" -eq 0 ]; then
+    for ((p=1; p <= CORES; p++)); do
+        echo -n "${p}\t"
+        for rep in {1..5}; do
+            EXEC_TIME="$( OMP_NUM_THREADS=$p "./"$PROG $PROB_SIZE $IT  | grep "Elapsed time:" | sed 's/Elapsed time: //' )"
+            echo -n "${EXEC_TIME}\t"
+        done
+        echo ""
+    done
+else
+    for rep in 1..$REP; do
+        EXEC_TIME="$( "./"$PROG $PROB_SIZE $IT | grep "Elapsed time:" | sed 's/Elapsed time: //' )"
+        echo -n "${EXEC_TIME}"
+    done
+fi
---
+++ Progetto-HPC-2023-2024/src/weak.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+# Questo script richiede 5 parametri per essere eseguito:
+# 1 - il nome del programma da eseguire
+# 2 - numero di ripetizioni
+# 3 - tipo di scaling (1 per prob. crescente, 0 per it. cresccente)
+# 4 - grandezza del problema base
+# 5 - numero di iterazioni base
+
+# Questo script esegue il programma sfruttando OpenMP con un numero 
+# di core da 1 al numero di core disponibili sulla macchina
+# (estremi inclusi). Il test con p processori viene effettuato su un
+# input che ha dimensione N0 * (p^(1/2)), dove N0 e' la dimensione
+# dell'input con p=1 thread OpenMP.
+#
+# Per come è stato implementato il programma parallelo, questo
+# significa che all'aumentare del numero p di thread OpenMP, la
+# dimensione del problema viene fatta crescere in modo che la quantità
+# di lavoro per thread resti costante.
+
+
+# Ultimo aggiornamento 2023-01-31
+# Moreno Marzolla (moreno.marzolla@unibo.it)
+
+PROG=$1
+REP=$2
+TYPE=$3
+N0=$4   # base problem size
+IT=$5   #iterations
+
+
+
+if [ ! -f "$PROG" ]; then
+    echo
+    echo "Non trovo il programma $PROG."
+    echo
+    exit 1
+fi
+
+echo -n "p\t"
+
+for t in seq $REP; do
+echo -n "t$t\t"
+done
+echo ""
+
+CORES=`cat /proc/cpuinfo | grep processor | wc -l` # number of cores
+
+for p in seq $CORES; do
+    echo -n "$p\t"
+    # Il comando bc non è in grado di valutare direttamente una radice
+    # cubica, che dobbiamo quindi calcolare mediante logaritmo ed
+    # esponenziale. L'espressione ($N0 * e(l($p)/2)) calcola
+    # $N0*($p^(1/2))
+    
+    PROB_SIZE=echo "$N0 * e(l($p)/2)" | bc -l -q
+    IT_SIZE=echo "$IT * $p" | bc -l -q
+
+    for rep in seq $REP; do
+        if [ "$TYPE" = 1 ]; then
+        EXEC_TIME="$( OMP_NUM_THREADS=$p "./"$PROG $PROB_SIZE $IT | grep "Elapsed time:" | sed 's/Elapsed time: //' )"
+        else
+        EXEC_TIME="$( OMP_NUM_THREADS=$p "./"$PROG $N0 $IT_SIZE | grep "Elapsed time:" | sed 's/Elapsed time: //' )"
+        fi
+        echo -n "${EXEC_TIME}\t"
+    done
+    echo ""
+done
